1ed33358f91f17a79d97e9b0d8328e4a8b79ab2c
maksim <krovorgen@gmail.com>
2022-04-15 22:47:30 +0300

    fixed

--- src/middleware/blogger-exists-middleware.ts
+++ src/middleware/blogger-exists-middleware.ts
@@ -1,14 +1,11 @@
-import { check } from 'express-validator';
 import { bloggers } from '../repositories/bloggers-repository';
 import { NextFunction, Request, Response } from 'express';
 
 export const bloggerExistsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  check('bloggerId').custom((value) => {
-    const isFounded = bloggers.find((user) => user.id === value);
-    if (!isFounded) {
-      res.sendStatus(404);
-    } else {
-      next();
-    }
-  });
+  const isFounded = bloggers.find((user) => user.id === req.body.bloggerId);
+  if (!isFounded) {
+    res.sendStatus(404);
+  } else {
+    next();
+  }
 };
